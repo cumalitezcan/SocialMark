6eae6e806db4c26513a47831fc60b69c59e36c78
Cumali Tezcan <34379535+cumalitezcan@users.noreply.github.com>
2021-09-16 16:56:13 +0300

    addedNavbar

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,9 @@ import router from './router';
 
 import "@/assets/style.css";
 
+import appHeader from "@/components/Shared/appHeader";
+
 const app = createApp(App);
+app.component("appHeader",appHeader);
 app.use(router);
 app.mount('#app');
