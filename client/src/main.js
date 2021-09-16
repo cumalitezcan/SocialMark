da8094607e1ac2db977b77d07f9ffc774bb1435a
Cumali Tezcan <34379535+cumalitezcan@users.noreply.github.com>
2021-09-16 18:05:09 +0300

    addedBookmarkListItem

--- client/src/main.js
+++ client/src/main.js
@@ -5,8 +5,10 @@ import router from './router';
 import "@/assets/style.css";
 
 import appHeader from "@/components/Shared/appHeader";
+import appBookmarkList from "@/components/Shared/appBookmarkList";
 
 const app = createApp(App);
-app.component("appHeader",appHeader);
+app.component("AppHeader",appHeader);
+app.component("appBookmarkList",appBookmarkList);
 app.use(router);
 app.mount('#app');
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,3 +1,24 @@
 <template>
-    <app-header />
+<div>
+<AppHeader />
+        <div class="flex flex-row">
+            <Sidebar />
+        <app-bookmark-list />
+        </div>
+</div>
+    
+       
+   
+
 </template>
+<script>
+  import Sidebar from "@/components/Home/Sidebar";
+  
+
+export default {
+    components:{
+        Sidebar
+    }
+}
+  
+</script>
